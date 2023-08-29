3cd0c93b4efa87f22da239012eaa0181551b5e3b
francoGrouiperre <francogroupierre98@gmail.com>
2023-08-29 00:21:45 -0300

    Tercer PreEntrega

--- Js/alegoritmoPrincipal.js
+++ Js/alegoritmoPrincipal.js
@@ -1,11 +1,43 @@
-let nombreDJ = document.getElementById("nombreDJ").value;
-let duracionDelSet = document.getElementById("duracionDelSet").value;
-let genero = document.getElementById("generos").value;
+let inputs = document.querySelectorAll("input");
 
+let formularioSet = document.getElementById("formularioSet");
+formularioSet.addEventListener("submit", guardarDatosIngresados);
+
+let nombreDJ = document.getElementById("Nombre del DJ").value;
+let genero = document.getElementById("generos").value;
+let duracionDelSet = document.getElementById("duracionDelSet").value;
+
+let setData = {
+   nombreDJ: nombreDJ,
+   genero: genero,
+   duracionDelSet: duracionDelSet
+};
+
+function guardarDatosIngresados(){
+   localStorage.setItem("set", JSON.stringify(setData));
+}
+
+function cantidadDeCanciones(duracionDelSet, genero){
+   switch(genero){
+      case "Tech House":
+         return Math.round(duracionDelSet/5);
+         break;
+      case "Melodic Techno":
+         return Math.round(duracionDelSet/6);
+         break;
+      case "Psytrance":
+         return Math.round(duracionDelSet/4);
+         break;
+      case "EDM":
+         return Math.round(duracionDelSet/3);
+         break; 
+   }
+   
+}
 class Set {
    constructor(nombreDJ, genero, duracionDelSet) {
       this.nombreDJ = nombreDJ.toUpperCase();
-      this.genero = genero.toUpperCase();
+      this.genero = genero;
       this.duracionDelSet = duracionDelSet;
    }
    hablar(){
@@ -17,8 +49,8 @@ const consultas = [];
 consultas.push(new Set(nombreDJ, genero, duracionDelSet));
 
 cantidadDeCanciones(duracionDelSet, genero); 
-const setsDeTH = consultas.filter((el) => el.genero == 1);
-const setsDeMT = consultas.filter((el) => el.genero == 2);
-const setsDePsy = consultas.filter((el) => el.genero == 3);
-const setsDeEDM = consultas.filter((el) => el.genero == 4);
+const setsDeTH = consultas.filter((el) => el.genero == "TechHouse");
+const setsDeMT = consultas.filter((el) => el.genero == "Melodic Techno");
+const setsDePsy = consultas.filter((el) => el.genero == "Psytrance");
+const setsDeEDM = consultas.filter((el) => el.genero == "EDM");
 
